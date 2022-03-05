f3db414bf41cc554adccba2cc9b0fa420e771471
Klára Madiová <madiovak@gmail.com>
2022-03-05 21:42:06 +0100

    Přidáno náhodné generování mince, vyřešen průnik panáčka a mince, zapisování skore, zvukové efekty

--- main.js
+++ main.js
@@ -9,9 +9,18 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
+
 function vratPx(pozice) {
 	return pozice + "px";
 }
+
+function prehraj(zvuk) {
+	zvuk.play();
+}
+
+
+
+
 
 class PanacekTrida {
 	constructor(panacekHTML) {
@@ -23,35 +32,114 @@ class PanacekTrida {
 	}
 
 	posunVlevo() {
-		this.x = this.x - this.panacekHTML.naturalWidth;
-		this.panacekHTML.style.left = vratPx(this.x);
-		document.getElementById('panacek').src = "obrazky/panacek-vlevo.png";
+
+		if (this.x > 0) {
+			this.x = this.x - this.panacekHTML.naturalWidth;
+			if (this.x < 0) {
+				this.x = 0;
+			}
+			this.panacekHTML.style.left = vratPx(this.x);
+			this.panacekHTML.src = "obrazky/panacek-vlevo.png";
+		}
 	}
 
+
 	posunVpravo() {
-		this.x = this.x + this.panacekHTML.naturalWidth;
-		this.panacekHTML.style.left = vratPx(this.x);
-		document.getElementById('panacek').src = "obrazky/panacek-vpravo.png";
+		// TODO zkusit opravit posuvník :/
+		if (this.x < window.innerWidth) {
+			this.x = this.x + this.panacekHTML.naturalWidth;
+			if (this.x > window.innerWidth) {
+				this.x = window.innerWidth - this.panacekHTML.naturalWidth;
+			}
+			this.panacekHTML.style.left = vratPx(this.x);
+			this.panacekHTML.src = "obrazky/panacek-vpravo.png";
+		}
 	}
 
 	posunNahoru() {
-		this.y = this.y - this.panacekHTML.naturalHeight;
-		this.panacekHTML.style.top = vratPx(this.y);
-		document.getElementById('panacek').src = "obrazky/panacek-nahoru.png";
+
+		if (this.y > 0) {
+			this.y = this.y - this.panacekHTML.naturalHeight;
+			if (this.y < 0) {
+				this.y = 0;
+			}
+			this.panacekHTML.style.top = vratPx(this.y);
+			this.panacekHTML.src = "obrazky/panacek-nahoru.png";
+		}
 	}
 
 	posunDolu() {
-		this.y = this.y + this.panacekHTML.naturalHeight;
-		this.panacekHTML.style.top = vratPx(this.y);
-		document.getElementById('panacek').src = "obrazky/panacek.png";
+
+		if (this.y < window.innerHeight) {
+			this.y = this.y + this.panacekHTML.naturalHeight;
+			if (this.y > window.innerHeight) {
+				this.y = window.innerHeight - this.panacekHTML.naturalHeight;
+			}
+			this.panacekHTML.style.top = vratPx(this.y);
+			this.panacekHTML.src = "obrazky/panacek.png";
+		}
 	}
 }
 
 
-let mujPanacek = new PanacekTrida(document.getElementById('panacek'));
-window.addEventListener('keydown', (e) => {
-	console.log('Stisknutí klávesy.');
 
+class MinceTrida {
+	constructor(minceHTML) {
+		this.minceHTML = minceHTML;
+		this.x = Math.floor(Math.random() * (window.innerWidth - minceHTML.naturalWidth));
+		this.y = Math.floor(Math.random() * (window.innerHeight - minceHTML.naturalHeight));
+		this.minceHTML.style.left = vratPx(this.x);
+		this.minceHTML.style.top = vratPx(this.y);
+	}
+
+}
+
+let hudba = document.querySelector("#hudba");
+let zvukMince = document.querySelector("#zvukmince");
+let fanfara = document.querySelector("#zvukfanfara");
+let scoreHTML = document.querySelector("#score");
+let score = 0;
+
+function prunik(panacek, mince) {
+	if (!(panacek.x + panacek.panacekHTML.naturalWidth < mince.x || mince.x + mince.minceHTML.naturalWidth < panacek.x || panacek.y + panacek.panacekHTML.naturalHeight < mince.y || mince.y + mince.minceHTML.naturalHeight < panacek.y)) {
+
+
+		prehraj(zvukMince);
+		if (score < 4) {
+			score++;
+			scoreHTML.textContent = score;
+			console.log(score);
+		} else if (score == 4) {
+			score++;
+			scoreHTML.textContent = score;
+			console.log("Fanfara" + score);
+			prehraj(fanfara);
+		} else {
+			score++;
+			scoreHTML.textContent = score;
+			console.log(score);
+		}
+
+
+
+		mince.x = Math.floor(Math.random() * (window.innerWidth - mince.minceHTML.naturalWidth));
+		mince.y = Math.floor(Math.random() * (window.innerHeight - mince.minceHTML.naturalHeight));
+
+		mince.minceHTML.style.left = vratPx(mince.x);
+		mince.minceHTML.style.top = vratPx(mince.y);
+
+
+	}
+}
+
+let mujPanacek = new PanacekTrida(document.getElementById('panacek'));
+let mojeMince = new MinceTrida(document.getElementById('mince'));
+
+
+
+
+
+window.addEventListener('keydown', (e) => {
 	switch (e.key) {
 		case 'ArrowDown':
 			mujPanacek.posunDolu();
@@ -66,7 +154,11 @@ window.addEventListener('keydown', (e) => {
 			mujPanacek.posunVpravo();
 			break;
 	}
+	prehraj(hudba);
+	prunik(mujPanacek, mojeMince)
 });
+
+
 
 
 
